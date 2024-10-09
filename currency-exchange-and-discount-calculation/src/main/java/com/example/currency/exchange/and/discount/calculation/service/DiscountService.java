b7082709e0d1fd0460468e3ddb6308669e3529e8
NikhilTripathi-dev <nikhiltripathi833@gmail.com>
2024-10-10 00:19:47 +0530

    Update DiscountService.java

--- currency-exchange-and-discount-calculation/src/main/java/com/example/currency/exchange/and/discount/calculation/service/DiscountService.java
+++ currency-exchange-and-discount-calculation/src/main/java/com/example/currency/exchange/and/discount/calculation/service/DiscountService.java
@@ -1,10 +1,6 @@
 package com.example.currency.exchange.and.discount.calculation.service;
-
-import org.springframework.stereotype.Service;
-
 import com.example.currency.exchange.and.discount.calculation.dto.BillRequest;
 
-@Service
 public interface DiscountService {
 
 	public double calculateDiscount(BillRequest billRequest);
