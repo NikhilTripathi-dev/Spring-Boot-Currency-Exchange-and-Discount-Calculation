dc7846425ea49feb73d9aeb7882a43d7ef8eee28
NikhilTripathi-dev <nikhiltripathi833@gmail.com>
2024-10-09 18:14:21 +0530

    Create CurrencyServiceImpl.java

---
+++ currency-exchange-and-discount-calculation/src/main/java/com/example/currency/exchange/and/discount/calculation/service/impl/CurrencyServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.currency.exchange.and.discount.calculation.service.impl;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.example.currency.exchange.and.discount.calculation.dto.ExchangeRateResponse;
+import com.example.currency.exchange.and.discount.calculation.service.CurrencyService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class CurrencyServiceImpl implements CurrencyService{
+
+	private final String API_KEY = "your-api-key";
+	private final String BASE_URL = "https://open.er-api.com/v6/latest/";
+	private final RestTemplate restTemplate;
+
+	@Override
+	@Cacheable(value = "exchangeRates", key = "#request.baseCurrency + '_' + #request.targetCurrency")
+	public double getExchangeRate(String baseCurrency, String targetCurrency) {
+		String url = UriComponentsBuilder.fromHttpUrl(BASE_URL + baseCurrency).queryParam("apikey", API_KEY)
+				.toUriString();
+		ExchangeRateResponse response = restTemplate.getForObject(url, ExchangeRateResponse.class);
+		if (response != null && response.getRates() != null && response.getRates().containsKey(targetCurrency)) {
+			return response.getRates().get(targetCurrency);
+		} else {
+			throw new RuntimeException("Failed to fetch exchange rate for " + targetCurrency);
+		}
+	}
+}
