b1c19d2371b5068c3d5d9f94bbf88fa5b932a7a8
NikhilTripathi-dev <nikhiltripathi833@gmail.com>
2024-10-09 23:27:49 +0530

    Update CurrencyController.java

--- currency-exchange-and-discount-calculation/src/main/java/com/example/currency/exchange/and/discount/calculation/controller/CurrencyController.java
+++ currency-exchange-and-discount-calculation/src/main/java/com/example/currency/exchange/and/discount/calculation/controller/CurrencyController.java
@@ -1,5 +1,9 @@
 package com.example.currency.exchange.and.discount.calculation.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,13 +20,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class CurrencyController {
 
-	private final CurrencyService currencyService;
+    private final CurrencyService currencyService;
     private final DiscountService discountService;
 
 	@PostMapping("/calculate")
-	public double calculate(@RequestBody BillRequest billRequest) {
+	public ResponseEntity<Map<String, Double>>  calculate(@RequestBody BillRequest billRequest) {
+		double discountTotal = discountService.calculateDiscount(billRequest);
 		double exchangeRate = currencyService.getExchangeRate(billRequest.getOriginalCurrency(), billRequest.getTargetCurrency());
-        return discountService.calculateTotalBill(billRequest, exchangeRate);
+		double total = discountTotal * exchangeRate;
+		Map<String, Double> response = new HashMap<>();
+	    response.put("total", total);
+	    return ResponseEntity.ok(response);
 	}
 
 }
