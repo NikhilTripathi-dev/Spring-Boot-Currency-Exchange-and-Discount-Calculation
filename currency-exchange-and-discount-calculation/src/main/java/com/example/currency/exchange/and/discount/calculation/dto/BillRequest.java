d3aee9be6dcf8f2d315d3dcabc5d56c8d8e3c049
NikhilTripathi-dev <nikhiltripathi833@gmail.com>
2024-10-09 18:07:33 +0530

    Update BillRequest.java

--- currency-exchange-and-discount-calculation/src/main/java/com/example/currency/exchange/and/discount/calculation/dto/BillRequest.java
+++ currency-exchange-and-discount-calculation/src/main/java/com/example/currency/exchange/and/discount/calculation/dto/BillRequest.java
@@ -1,19 +1,27 @@
 package com.example.currency.exchange.and.discount.calculation.dto;
 
+import java.util.List;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class BillRequest {
-	
+
 	private double totalAmount;
-	private String userType;
-	private boolean isGrocery;
-	private int customerTenure;
+	private List<Product> products;
+	private User user;
 	private String originalCurrency;
 	private String targetCurrency;
+	
+	 public BillRequest(User user, List<Product> products, String originalCurrency, String targetCurrency) {
+	        this.user = user;
+	        this.products = products;
+	        this.originalCurrency = originalCurrency;
+	        this.targetCurrency = targetCurrency;
+	    }
 
 }
