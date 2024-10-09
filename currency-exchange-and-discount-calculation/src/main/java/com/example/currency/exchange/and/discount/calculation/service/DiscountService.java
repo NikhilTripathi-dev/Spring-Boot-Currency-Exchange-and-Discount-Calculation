b13cea28ec91b8fe9d0d7bf97cb6667fb2500d6f
NikhilTripathi-dev <nikhiltripathi833@gmail.com>
2024-10-09 18:12:10 +0530

    Update DiscountService.java

--- currency-exchange-and-discount-calculation/src/main/java/com/example/currency/exchange/and/discount/calculation/service/DiscountService.java
+++ currency-exchange-and-discount-calculation/src/main/java/com/example/currency/exchange/and/discount/calculation/service/DiscountService.java
@@ -2,32 +2,12 @@ package com.example.currency.exchange.and.discount.calculation.service;
 
 import org.springframework.stereotype.Service;
 
+import com.example.currency.exchange.and.discount.calculation.dto.BillRequest;
+
 @Service
-public class DiscountService {
+public interface DiscountService {
 
-	public double calculateDiscount(double totalAmount, String userType, boolean isGrocery, int customerTenure) {
-		double discount = 0;
-
-		if (!isGrocery) {
-			switch (userType) {
-			case "EMPLOYEE":
-				discount = 0.30;
-				break;
-			case "AFFILIATE":
-				discount = 0.10;
-				break;
-			case "LOYAL_CUSTOMER":
-				if (customerTenure > 2) {
-					discount = 0.05;
-				}
-				break;
-			}
-		}
-
-		int hundredBlocks = (int) (totalAmount / 100);
-		double flatDiscount = hundredBlocks * 5;
-
-		 return isGrocery ? totalAmount : (totalAmount * (1 - discount) - flatDiscount);
-	}
+	public double calculateTotalBill(BillRequest billRequest, double exchangeRate);
+		
 
 }
