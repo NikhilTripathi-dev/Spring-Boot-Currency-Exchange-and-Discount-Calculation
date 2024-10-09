98d050ab82f6bbcf82b93594ad249c8710e4b7d7
NikhilTripathi-dev <nikhiltripathi833@gmail.com>
2024-10-10 00:20:10 +0530

    Update CurrencyService.java

--- currency-exchange-and-discount-calculation/src/main/java/com/example/currency/exchange/and/discount/calculation/service/CurrencyService.java
+++ currency-exchange-and-discount-calculation/src/main/java/com/example/currency/exchange/and/discount/calculation/service/CurrencyService.java
@@ -1,8 +1,5 @@
 package com.example.currency.exchange.and.discount.calculation.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface CurrencyService{
 	
 	public double getExchangeRate(String baseCurrency, String targetCurrency);
