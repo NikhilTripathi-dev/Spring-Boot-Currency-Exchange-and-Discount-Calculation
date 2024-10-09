5b44ca47ee23073491cf07e2901385d14074520b
NikhilTripathi-dev <nikhiltripathi833@gmail.com>
2024-10-09 23:34:43 +0530

    Update CurrencyExchangeAndDiscountCalculationApplicationTests.java

--- currency-exchange-and-discount-calculation/src/test/java/com/example/currency/exchange/and/discount/calculation/CurrencyExchangeAndDiscountCalculationApplicationTests.java
+++ currency-exchange-and-discount-calculation/src/test/java/com/example/currency/exchange/and/discount/calculation/CurrencyExchangeAndDiscountCalculationApplicationTests.java
@@ -2,12 +2,23 @@ package com.example.currency.exchange.and.discount.calculation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
-@SpringBootTest(classes = CurrencyExchangeAndDiscountCalculationApplication.class)
+import com.example.currency.exchange.and.discount.calculation.service.CurrencyService;
+import com.example.currency.exchange.and.discount.calculation.service.DiscountService;
+
+@SpringBootTest
 class CurrencyExchangeAndDiscountCalculationApplicationTests {
+
+	@MockBean
+	private CurrencyService currencyService;
+
+	@MockBean
+	private DiscountService discountService;
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
