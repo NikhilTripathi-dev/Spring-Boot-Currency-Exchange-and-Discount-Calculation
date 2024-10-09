3f49f8745d6c1919e6bb32c2765e81eb45a80f8d
NikhilTripathi-dev <nikhiltripathi833@gmail.com>
2024-10-09 13:47:40 +0530

    Update CurrencyControllerTest.java

--- currency-exchange-and-discount-calculation/src/test/java/com/example/currency/exchange/and/discount/calculation/controller/CurrencyControllerTest.java
+++ currency-exchange-and-discount-calculation/src/test/java/com/example/currency/exchange/and/discount/calculation/controller/CurrencyControllerTest.java
@@ -1,24 +1,28 @@
 package com.example.currency.exchange.and.discount.calculation.controller;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.ResponseEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.currency.exchange.and.discount.calculation.dto.BillRequest;
 import com.example.currency.exchange.and.discount.calculation.service.CurrencyService;
 import com.example.currency.exchange.and.discount.calculation.service.DiscountService;
 
+@SpringBootTest 
+@AutoConfigureMockMvc
 public class CurrencyControllerTest {
 
-    @InjectMocks
-    private CurrencyController currencyController;
-
+    @Autowired
+    private MockMvc mockMvc;
+    
     @Mock
     private CurrencyService currencyService;
 
@@ -27,22 +31,19 @@ public class CurrencyControllerTest {
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
+    	 MockitoAnnotations.openMocks(this);
+         when(discountService.calculateDiscount(150.0, "EMPLOYEE", false, 3)).thenReturn(135.0);
+         when(currencyService.getExchangeRate("USD", "EUR")).thenReturn(0.85);
     }
 
     @Test
-    public void calculate_ShouldReturnPayableAmount() {
-       
-        BillRequest request = new BillRequest();
-        request.setTotalAmount(100.0);         request.setUserType("EMPLOYEE"); 
-        request.setGrocery(false); 
-        request.setCustomerTenure(3); 
-        request.setOriginalCurrency("USD");
-        request.setTargetCurrency("EUR");
-        when(discountService.calculateDiscount(100.0, "EMPLOYEE", false, 3)).thenReturn(70.0);
-        when(currencyService.getExchangeRate("USD", "EUR")).thenReturn(0.85);
+    public void testCalculatePayableAmount() throws Exception {
+        String requestBody = "{\"totalAmount\": 150.0, \"userType\": \"EMPLOYEE\", \"isGrocery\": false, \"customerTenure\": 3, \"originalCurrency\": \"USD\", \"targetCurrency\": \"EUR\"}";
 
-        ResponseEntity<Double> response = currencyController.calculate(request);
-        assertEquals(ResponseEntity.ok(59.5), response);
+        mockMvc.perform(post("/api/calculate")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody)
+        )
+        .andExpect(status().isOk());
     }
 }
